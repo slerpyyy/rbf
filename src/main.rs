e4e114b720c1c9cd83cf70f2009f410507f07df4
slerpy <48803074+slerpyyy@users.noreply.github.com>
2020-02-21 13:15:44 +0100

    Rewrote all cmd args logic
    * Moved from "clap" to "getopts"
    * added -i option

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,9 @@ use std::io;
 use std::io::prelude::*;
 use std::fs::File;
 use std::collections::*;
-use clap::*;
+use string::*;
+use stringreader::*;
+use getopts::*;
 
 mod internal;
 mod parser;
@@ -13,71 +15,80 @@ use internal::*;
 use parser::*;
 use evaluator::*;
 
+fn print_usage(program : &str, opts : Options) {
+	println!("Usage: {} FILE [options]\n", program);
+	print!("A simple optimizing Brainfuck interpreter written in Rust.");
+	println!("{}", opts.usage(""));
+}
+
 fn main() -> io::Result<()> {
+	let args: Vec<String> = env::args().collect();
+	let program = args[0].clone();
 
-	let matches = App::new("rbf")
-		.version("0.1.0")
-		.about("A simple optimizing Brainfuck interpreter written in Rust.")
-		.arg(Arg::with_name("file")
-			.value_name("FILE")
-			.help("Sets the input file to use")
-			.required(false)
-			.index(1))
-		.arg(Arg::with_name("command")
-			.short("c")
-			.long("cmd")
-			.value_name("CODE")
-			.help("Sets a custom config file")
-			.takes_value(true))
-		.arg(Arg::with_name("verbosity")
-			.short("v")
-			.multiple(true)
-			.help("Sets the level of verbosity"))
-		.get_matches();
+	let mut opts = Options::new();
+	opts.optflag("h", "help", "print this help message");
+	opts.optopt("c", "cmd", "run code from command", "CODE");
+	opts.optopt("i", "input", "use string as input", "STRING");
+	opts.optflagmulti("v", "verbose", "increase level of verbosity");
 
-	let command = matches.value_of("command");
-	let filename = matches.value_of("file");
-	let verbosity = matches.occurrences_of("verbosity");
+	let matches = match opts.parse(&args[1..]) {
+		Ok(m) => { m },
+		Err(f) => { panic!(f.to_string()) },
+	};
 
-	let mut buffer = Vec::new();
+	if matches.opt_present("h") {
+		print_usage(&program, opts);
+		return Ok(());
+	}
 
-	if let Some(cmd_str) = command {
-		buffer = cmd_str.as_bytes().to_vec();
+	let verbosity = matches.opt_count("v");
+
+	let mut code : Vec<u8> = Vec::new();
+
+	if let Some(command) = matches.opt_str("c") {
+		code = command.as_bytes().to_vec();
 	} else {
-		if let Some(filename_str) = filename {
-			if filename_str == "-" {
-				io::stdin().read_to_end(&mut buffer)?;
+		if let Some(file_name) = matches.free.get(0) {
+			if file_name == "-" {
+				io::stdin().read_to_end(&mut code)?;
 			} else {
-				let file = File::open(filename_str);
+				let file = File::open(file_name);
 
 				if let Err(err) = file {
 					eprintln!("ERROR : {}", err);
-					std::process::exit(-1);
+					process::exit(-1);
 				}
 
 				let mut raw_file = file?;
-				raw_file.read_to_end(&mut buffer)?;
+				raw_file.read_to_end(&mut code)?;
 			}
 		} else {
-			eprintln!("ERROR : No input found");
-			std::process::exit(-1);
+			print_usage(&program, opts);
+			return Ok(());
 		}
+	};
+
+	let stdin = std::io::stdin();
+	let stdout = std::io::stdout();
+
+	let mut input : &mut dyn Read = &mut stdin.lock();
+	let output : &mut dyn Write = &mut stdout.lock();
+
+	let in_string_default = "".to_string();
+	let in_string = matches.opt_str("i").unwrap_or(in_string_default.clone());
+	let mut str_input = StringReader::new(&in_string[..]);
+
+	if in_string != in_string_default {
+		input = &mut str_input;
 	}
 
-
-	let (prog, _) = parse(&buffer, 0);
-
-	if verbosity > 0 {
-		show_code(&prog, 0);
-	}
+	let (prog, _) = parse(&code, 0);
+	if verbosity > 0 { show_code(&prog, 0); }
 
 	let mut tape = VecDeque::with_capacity(0x10000);
 	for _ in 0..0x100 { tape.push_back(0u8); }
 
-	let input = std::io::stdin();
-	let output = std::io::stdout();
+	eval(&prog, input, output, &mut tape, 0)?;
 
-	eval(&prog, &mut input.lock(), &mut output.lock(), &mut tape, 0)?;
-
-	io::Result::Ok(())
+	Ok(())
 }
